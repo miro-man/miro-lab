c441ccffa37b3176661d468ce45dbe8b6ace110b
MIRO MAN <abderaheman207@gmail.com>
2025-09-03 21:13:55 +0100

    feat: إنشاء الهيكل الأساسي لموقع MIRO MAN (index.html, style.css, script.js)

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// في المستقبل يمكن إضافة تفاعلات ديناميكية هنا
-console.log("مرحباً بك في موقعي الشخصي!");
+// مثال صغير: تمرير سلس عند الضغط على الروابط
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
